07a24b19f8e5391d5a12a8ccfb925594e8ca3825
mclick <mclick@ucsc.edu>
2021-10-30 09:43:23 -0700

    Update PlayerController.cs
    Made character rotate with mouse movement rather than with the space bar.

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -7,6 +7,12 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+	// horizontal rotation speed
+    public float horizontalSpeed = 1f;
+    // vertical rotation speed
+    public float verticalSpeed = 1f;
+    private float xRotation = 0.0f;
+    private float yRotation = 0.0f;
     public float speed = 1.0f;
     public float camAngle = 0.0f;
     public GameObject firstCam;
@@ -21,14 +27,17 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         // Rotate moveVector by the direction player is facing.
-        Vector3 moveVector = Quaternion.Euler(0.0f, -1.0f * (this.camAngle / Mathf.PI) * 180, 0.0f) * new Vector3(Input.GetAxis("Horizontal") * this.speed, 0.0f, Input.GetAxis("Vertical") * this.speed);
+        Vector3 moveVector = Quaternion.Euler(0.0f, yRotation, 0.0f) * new Vector3(Input.GetAxis("Horizontal") * this.speed, 0.0f, Input.GetAxis("Vertical") * this.speed);
         this.GetComponent<CharacterController>().SimpleMove(moveVector);
 
         // Rotate Camera
-        var camTransTwo = this.firstCam.GetComponent<Transform>();
-        if (Input.GetKey(KeyCode.Space)){
-            this.camAngle += 0.01f;
-            camTransTwo.localRotation = Quaternion.Euler(new Vector3(10.0f, -1.0f * (this.camAngle / Mathf.PI) * 180, 0.0f));
-        }
+		float mouseX = Input.GetAxis("Mouse X") * horizontalSpeed;
+        float mouseY = Input.GetAxis("Mouse Y") * verticalSpeed;
+ 
+        yRotation += mouseX;
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90, 90);
+ 
+        firstCam.transform.eulerAngles = new Vector3(xRotation, yRotation, 0.0f);
     }
 }
