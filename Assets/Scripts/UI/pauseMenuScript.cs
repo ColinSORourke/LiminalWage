740c5e4adc7fdaef51c85d9e20d8c19efb1f2733
mclick <86328232+mclick@users.noreply.github.com>
2021-11-05 17:16:57 -0700

    removed duplicated using statements in pauseMenuScript

--- Assets/Scripts/UI/pauseMenuScript.cs
+++ Assets/Scripts/UI/pauseMenuScript.cs
@@ -2,9 +2,6 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 
 public class pauseMenuScript : MonoBehaviour
 {
